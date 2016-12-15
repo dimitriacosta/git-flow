e6ac15c68160946ef907a1cdc14ade3ef9130aff
Dimitri Acosta <acosta.dimitri@gmail.com>
2016-12-15 10:12:46 -0700

    Authenticate users with username instead of email

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,7 @@
 
                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
+                                            <button type="submit">Logout</button>
                                         </form>
                                     </li>
                                 </ul>
---
+++ tests/features/AuthTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $name = 'John Doe';
+    private $email = 'john@example.com';
+    private $username = 'johndoe';
+    private $password = 'secret';
+    private $user;
+
+    /** @test */
+    public function create_a_new_user()
+    {
+        $this->visit('/register')
+            ->type($this->name, 'name')
+            ->type($this->email, 'email')
+            ->type($this->username, 'username')
+            ->type($this->password, 'password')
+            ->type($this->password, 'password_confirmation')
+            ->press('Register')
+            ->seeCredentials([
+                'name' => $this->name,
+                'email' => $this->email,
+                'username' => $this->username,
+                'password' => $this->password,
+            ])
+            ->seeIsAuthenticated()
+            ->seePageIs('/home');
+    }
+
+    private function user(array $attributes = [])
+    {
+        if (!is_null($this->user)) {
+            return $this->user;
+        }
+
+        if (!empty($attributes['password'])) {
+            $attributes['password'] = bcrypt($attributes['password']);
+        }
+
+        return $this->user = factory(User::class)->create($attributes);
+    }
+
+    /** @test */
+    public function a_user_can_log_in()
+    {
+        $this->user([
+            'username' => $this->username,
+            'password' => $this->password,
+        ]);
+
+        $this->visit('/login')
+            ->type($this->username, 'username')
+            ->type($this->password, 'password')
+            ->press('Login')
+            ->seeCredentials([
+                'username' => $this->username,
+                'password' => $this->password,
+            ])
+            ->seeIsAuthenticated()
+            ->seePageIs('/home');
+    }
+
+    /** @test */
+    public function users_can_log_out()
+    {
+        $user = $this->user();
+
+        $this->actingAs($user)
+            ->visit('/home')
+            ->press('Logout')
+            ->dontSeeIsAuthenticated()
+            ->seePageIs('/');
+    }
+}
